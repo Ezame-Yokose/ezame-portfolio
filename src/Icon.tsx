fa827b568c64fedc3f5f5dfc4903d1e4440a2db5
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2022-04-21 22:52:23 +0900

    edit: Icon.tsx

--- src/Icon.tsx
+++ src/Icon.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import EzameImage from './ezame.jpg'
+import './Icon.css'
 
 const Icon = () => {
   return (
     <div>
       <img src={EzameImage} alt='Ezame icon'/>
       <h1>Ezame</h1>
+      <p>@Ez4meliy</p>
     </div>
   )
 }
